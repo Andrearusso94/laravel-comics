5a47500ae374dd5fe68175e4232ebbe43a08472c
Andrearusso94 <andrea.r.4845@gmail.com>
2022-12-16 16:04:59 +0100

    add commit img and header.blade

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light  bg-light">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="" class="my-2">
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarText">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+
+                        <a class="nav-link text-dark" href=""></a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
